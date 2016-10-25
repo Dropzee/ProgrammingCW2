927e7750c370ec097d429532638a7a22fd186c90
Dropzee <jackcox@ntlworld.com>
2016-10-25 15:09:25 +0100

    [+] Error insertion

--- ProgrammingCW2/ProgrammingCW2/Decoder.h
+++ ProgrammingCW2/ProgrammingCW2/Decoder.h
@@ -1,8 +1,14 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
+
 class Decoder
 {
 public:
 	Decoder();
 	~Decoder();
+	void insertError(string* s);
+	string decode(string input);
 };
 
--- ProgrammingCW2/ProgrammingCW2/FileIO.h
+++ ProgrammingCW2/ProgrammingCW2/FileIO.h
@@ -1,8 +1,17 @@
 #pragma once
+#include <string>
+#include <stdexcept>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
 class FileIO
 {
 public:
 	FileIO();
 	~FileIO();
+	string read(string path);
+	void read(vector<string> &files, string folder);
 };
 
